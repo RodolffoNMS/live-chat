50e1739a0e460e3b2f4639140b632c38ffc8932d
matheus <matheus.pereira@zup.com.br>
2025-05-21 11:34:06 -0300

    feat: add output for public_subnet_b

--- src/main/java/zup/live_chat/infra/output.tf
+++ src/main/java/zup/live_chat/infra/output.tf
@@ -2,8 +2,12 @@ output "vpc_id" {
   value = aws_vpc.livechat-vpc.id
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public.id
+output "public_subnet_a_id" {
+  value = aws_subnet.public_a.id
+}
+
+output "public_subnet_b_id" {
+  value = aws_subnet.public_a.id
 }
 
 output "private_subnet_id" {
