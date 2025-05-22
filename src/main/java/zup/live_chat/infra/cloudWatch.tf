17cb5dd91ed305caf5a6e33d2cb0d57fa53067ae
matheus <matheus.pereira@zup.com.br>
2025-05-22 18:47:23 -0300

    feat: add cloudwatch logs for ecs

--- src/main/java/zup/live_chat/infra/cloudWatch.tf
+++ src/main/java/zup/live_chat/infra/cloudWatch.tf
@@ -2,3 +2,8 @@ resource "aws_cloudwatch_log_group" "livechat_apigateway" {
   name = "/aws/apigateway/livechat"
   retention_in_days = 7
 }
+
+resource "aws_cloudwatch_log_group" "ecs_livechat" {
+  name              = "/ecs/my-app"
+  retention_in_days = 7
+}
